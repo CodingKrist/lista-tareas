f3cf05db2cbe178e50870caa01ec660c68603f27
CodingKrist <122282123+CodingKrist@users.noreply.github.com>
2024-03-27 21:18:29 +0100

    fixing some style and creating login and signup ejs

--- routes/main.js
+++ routes/main.js
@@ -4,5 +4,8 @@ const homeController = require('../controllers/home')
 
 
 router.get('/', homeController.getIndex)
+router.get('/login', homeController.getLogin)
+router.get('/signup', homeController.getSignUp)
+
 
 module.exports = router
